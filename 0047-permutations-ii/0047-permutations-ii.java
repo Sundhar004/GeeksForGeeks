771c177997e37b61a711e49043f56068cd487d15
Sundhar004 <sundhararameshwar.senthil@gmail.com>
2026-08-04 16:07:30 +0530

    Time: 3 ms (91.42%), Space: 46.8 MB (44.21%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     List<List<Integer>> ans = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
         boolean used[] = new boolean[nums.length];
         permutation(nums,new ArrayList<>(),used);
         return ans;
@@ -9,17 +10,17 @@ class Solution {
     {
         if(curr.size() == nums.length)
         {
-            if(!ans.contains(curr)) ans.add(new ArrayList<>(curr));
+            ans.add(new ArrayList<>(curr));
             return;
         }
         for(int i=0;i<nums.length;i++)
         {
             if(used[i]) continue;
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
             used[i] = true;
             curr.add(nums[i]);
             permutation(nums,curr,used);
             curr.remove(curr.size()-1);
-            permutation(nums,curr,used);
             used[i] = false;
         }
     }
